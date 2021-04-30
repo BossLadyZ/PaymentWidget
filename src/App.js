ba4b50bdf37e9ee40145c433c56fc294cbd41ce8
Azeezat Busari <BossLadyZ@users.noreply.github.com>
2021-04-30 16:15:17 +0100

    another one

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
+import  apiTest  from './apiTest'
 
 import { MultiStepForm } from './components/MultiStepForm'
 import { Opening } from './Opening';
@@ -13,7 +13,9 @@ function App() {
   return (
   <Router>
   <Switch>
+      
       <Route exact path="/" component={Opening} />
+      <Route exact path="/api" component={apiTest} />
       <Route path="/details" component={MultiStepForm} />
   </Switch>
 </Router>
