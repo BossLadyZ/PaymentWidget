978f949d631fd0ec4ba40752c1b206e8d6e68e33
Azeezat Busari <BossLadyZ@users.noreply.github.com>
2021-04-30 14:27:47 +0100

    Commits as the project was being developed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import { MultiStepForm } from './components/MultiStepForm'
+import { Opening } from './Opening';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <Router>
+  <Switch>
+      <Route exact path="/" component={Opening} />
+      <Route path="/details" component={MultiStepForm} />
+  </Switch>
+</Router>
+  )
 }
 
 export default App;
+/** return (
+    <div className="App">
+      <Opening />
+     <MultiStepForm />
+    </div>
+  ); */
